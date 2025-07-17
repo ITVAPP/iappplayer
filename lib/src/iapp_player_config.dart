31a816909371677e7537dfc2a5d237ef97444d19
电视宝 www.itvapp.net <service@itvapp.net>
2025-07-18 03:35:18 +0800

    Update iapp_player_config.dart

--- lib/src/iapp_player_config.dart
+++ lib/src/iapp_player_config.dart
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2024 itvapp.net and iapp_player contributors
+ * Licensed under the Apache License, Version 2.0
+ */
 import 'dart:collection';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
